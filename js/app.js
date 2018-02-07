da4b4d130899cab980817b2687aff80d965b5e53
John Tran <john.kn.tran@gmail.com>
2018-02-07 17:59:02 -0500

    Added responsive chart with Chart.js to application.

--- js/app.js
+++ js/app.js
@@ -6,12 +6,13 @@
 
         // Change this API endpoint according to your own backend.
         // Mine is deployed on a Google Cloud instance, but your endpoint URL will be different.
-        let apiEndpoint = 'http://35.227.108.253/api/twitter_search.py';
+        const apiEndpoint = 'http://35.227.108.253/api/twitter_search.py';
 
         $http.get(apiEndpoint).then(
             response => {
                 let ajaxRes = response.data.result;
                 $scope.tweets = ajaxRes ? response.data.payload : [];
+                makeChart();
             }
         );
 
@@ -22,6 +23,7 @@
                     response => {
                         let ajaxRes = response.data.result;
                         $scope.tweets = ajaxRes ? response.data.payload: [];
+                        makeChart();
                         let twitterTimeline = angular.element(document.querySelector('.twitter-timeline'));
                         twitterTimeline.replaceWith(`<a class="twitter-timeline" data-width="420" data-height="760" data-theme="light" data-link-color="#FAB81E" href="https://twitter.com/${$scope.searchQuery}">Tweets by ${$scope.searchQuery}</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>`);
                     }
@@ -29,5 +31,57 @@
             }
         };
 
+        const makeChart = () => {
+            let num_words = $scope.tweets.map(twt => twt.num_words);
+            let dates = $scope.tweets.map(twt => twt.time);
+            let context = document.getElementById("tweetGraph").getContext("2d");
+            let chart = new Chart(context, {
+                type: "line",
+                data: {
+                    labels: [1,2,3,4,5,6].map(num => "#" + num),
+                    datasets: [{
+                        label: "",
+                        backgroundColor: "rgb(43, 196, 79)",
+                        borderColor: "rgb(71, 171, 94)",
+                        data: num_words,
+                        fill: false,
+                    }]
+                },
+                options: {
+                    responsive: true,
+                    title:{
+                        display:true,
+                        text:"Total Words per Tweet over Time"
+                    },
+                    tooltips: {
+                        mode: "index",
+                        intersect: false,
+                    },
+                    hover: {
+                        mode: "nearest",
+                        intersect: true
+                    },
+                    scales: {
+                        xAxes: [{
+                            display: true,
+                            scaleLabel: {
+                                display: true,
+                                labelString: "Tweets"
+                            }
+                        }],
+                        yAxes: [{
+                            display: true,
+                            scaleLabel: {
+                                display: true,
+                                labelString: "Words"
+                            },
+                            ticks: {
+                                beginAtZero:true
+                            }
+                        }]
+                    }
+                }
+            });
+        }
     });
 })();
